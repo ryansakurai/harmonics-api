980cf5a23edb278e39ab409755174bcdc23bea79
Ryan Sakurai <ryansakurai03@gmail.com>
2025-08-23 23:53:00 -0300

    chore: migrate to pyproject.toml

--- src/harmonics_api/main.py
+++ src/harmonics_api/main.py
@@ -5,17 +5,20 @@ from flask import Flask
 from harmonics_api.configs import mongodb, neo4j
 from harmonics_api.routes import artists, releases, users, recs
 
-app = Flask("Harmonics API")
-app.json.sort_keys = False
-app.url_map.strict_slashes = False
+def main() -> None:
+    app = Flask("Harmonics API")
+    app.json.sort_keys = False
+    app.url_map.strict_slashes = False
 
-app.register_blueprint(artists.bp, url_prefix = "/v1/artists")
-app.register_blueprint(releases.bp, url_prefix = "/v1/releases")
-app.register_blueprint(users.bp, url_prefix = "/v1/users")
-app.register_blueprint(recs.bp, url_prefix = "/v1/recs")
+    app.register_blueprint(artists.bp, url_prefix = "/v1/artists")
+    app.register_blueprint(releases.bp, url_prefix = "/v1/releases")
+    app.register_blueprint(users.bp, url_prefix = "/v1/users")
+    app.register_blueprint(recs.bp, url_prefix = "/v1/recs")
 
-if __name__=="__main__":
     app.run(debug = True)
 
     mongodb.client.close()
     neo4j.driver.close()
+
+if __name__=="__main__":
+    main()
