5338ead73815a74dcaa78bef7a1c4566418a1e06
Ryan Sakurai <ryansakurai03@gmail.com>
2025-10-27 21:55:58 -0300

    refactor!: erase Python code and init Go project

--- api/src/harmonics_api/common/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Common modules for all routes of the API."""
-
-from . import errors, helper, mongodb, neo4j
-
-__all__ = ["errors", "helper", "mongodb", "neo4j"]
--- api/src/harmonics_api/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-"""API route modules."""
-
-from . import artists, recs, releases, users
-
-__all__ = ["artists", "recs", "releases", "users"]
